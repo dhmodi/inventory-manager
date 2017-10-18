0b9fdc7d700ad37bf2cbe70d107dd6074058e192
dhmodi <dhavalmodi24@gmail.com>
2017-10-18 15:23:22 +0530

    d

--- app.py
+++ app.py
@@ -313,7 +313,7 @@ def processRequest(req):
         # cur = conn.cursor()
         # cur.execute(queryString)
         # rows = cur.fetchall()
-        socketio.emit('chartdata', { "type": "line", "source": [{ "label": "Mon", "value": "15123" }, { "label": "Tue", "value": "14233" }, { "label": "Wed", "value": "23507" }, { "label": "Thu", "value": "9110" }, { "label": "Fri", "value": "15529" }, { "label": "Sat", "value": "20803" }, { "label": "Sun", "value": "19202" } ] })
+        socketio.emit('chartdata', [{ "type": "line", "source": [{ "label": "Mon", "value": "15123" }, { "label": "Tue", "value": "14233" }, { "label": "Wed", "value": "23507" }, { "label": "Thu", "value": "9110" }, { "label": "Fri", "value": "15529" }, { "label": "Sat", "value": "20803" }, { "label": "Sun", "value": "19202" } ] }])
         outText = "Check it on right"
         return {
             "speech": outText,
