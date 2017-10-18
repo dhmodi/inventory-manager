ab355df19258aacea2910b30d24f1564cc0aab5b
dhmodi <dhavalmodi24@gmail.com>
2017-10-18 17:22:46 +0530

    d

--- app.py
+++ app.py
@@ -304,7 +304,7 @@ def processRequest(req):
         #     value = str(row).split(",")[1]
         #     print(label, value)
         #     chartData = chartData + '{ "label": "' + label + '", "value": "' + value + '"}'
-        chartData = [{"label": str(row).split(",")[0], "value": str(row).split(",")[1]} for row in rows]
+        chartData = [{"label": row.split(",")[0], "value": row.split(",")[1]} for row in rows]
         print (chartData)
         chartData = json.dumps(chartData)
         # final_json = '[ { "type":"' + chartType + '", "chartcontainer":"barchart", "source":[ { "label": "Mon", "value": "15123" }, { "label": "Tue", "value": "14233" }, { "label": "Wed", "value": "23507" }, { "label": "Thu", "value": "9110" }, { "label": "Fri", "value": "15529" }, { "label": "Sat", "value": "20803" }, { "label": "Sun", "value": "19202" } ]}]'
