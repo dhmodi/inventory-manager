b5f2b6514043dd29143111f294ac617dea40fdc5
dhmodi <dhavalmodi24@gmail.com>
2017-11-13 08:51:24 +0530

    d

--- app.py
+++ app.py
@@ -301,17 +301,17 @@ def processRequest(req):
                 if (columns[count][0] is not None):
                     # print(columns)
                     column = columns[count][0].split('.')[1]
-                    operation = columns[count][1]
-                    if (operation is None):
-                        print("The Operation is None")
-                        outText = outText + column + " is " + value
-                    elif (operation is "COUNT"):
-                        print("The Operation is " + str(operation))
-                        outText = outText + operation + " of " + table + " is " + value
-                    else:
-                        print("The Operation is " + str(operation))
-                        outText = outText + operation + " of " + column + " is " + value
-                if (isLast is not 0):
+                operation = columns[count][1]
+                if (operation is None):
+                    print("The Operation is None")
+                    outText = outText + column + " is " + value
+                elif (operation is "COUNT"):
+                    print("The Operation is " + str(operation))
+                    outText = outText + operation + " of " + table + " is " + value
+                else:
+                    print("The Operation is " + str(operation))
+                    outText = outText + operation + " of " + column + " is " + value
+            if (isLast is not 0):
                     outText = outText + " and the "
                 count = count + 1
         # print(','.join(str(element) for row in rows for element in row))
