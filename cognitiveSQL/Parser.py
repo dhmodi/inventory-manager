8220f1d4f4c94429a5f31fc2b948989e29734029
dhmodi <dhaval.modi@hpe.com>
2017-12-16 17:55:09 +0530

    d

--- cognitiveSQL/Parser.py
+++ cognitiveSQL/Parser.py
@@ -565,7 +565,7 @@ class Parser:
         from_phrase = ''
         where_phrase = ''
 
-        hashColumn_csv = 'cognitive/alias/synonyms.csv'
+        hashColumn_csv = 'cognitiveSQL/alias/synonyms.csv'
         input_sentence = hashMap_columns(sentence, hashColumn_csv)
         print(input_sentence)
         words = re.findall(r"[\w]+", self.remove_accents(input_sentence))
